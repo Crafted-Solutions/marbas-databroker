51dbf1cefc923e809a1200dfb4f25e844ebcf590
hatelamers <97636078+hatelamers@users.noreply.github.com>
2025-03-21 18:35:16 +0100

    #52 added ability to publsh binary releases

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # marbas-databroker
-![Runs on Windows](https://img.shields.io/badge/_%E2%9C%94-Win-black) ![Runs on MacOS](https://img.shields.io/badge/_%E2%9C%94-Mac-black) ![Runs on Linux](https://img.shields.io/badge/_%E2%9C%94-Linux-black) ![Tool](https://img.shields.io/badge/.Net-8-lightblue)
+![Runs on Windows](https://img.shields.io/badge/_%E2%9C%94-Win-black) ![Runs on MacOS](https://img.shields.io/badge/_%E2%9C%94-Mac-black) ![Runs on Linux](https://img.shields.io/badge/_%E2%9C%94-Linux-black) ![Tool](https://img.shields.io/badge/.Net-8-lightblue) [<img src="https://img.shields.io/github/v/release/Crafted-Solutions/marbas-databroker" title="Latest">](../../releases/latest)
 
 Data schema management libraries for MarBas system.
 
@@ -19,7 +19,25 @@ Execute in the solution directory
 dotnet run --project src/MarBasAPI/MarBasAPI.csproj
 ```
 
-API is then available at https://localhost:7277/swagger/index.html. Endpoints require authentication, for testing purposes dummy basic auth is turned on. In Swagger go to "Authorize" and login using arbitrary user name with password "*b*"
+Swagger test app is then available at https://localhost:7277/swagger/index.html and API - at https://localhost:7277/api/marbas. Endpoints require authentication, for testing purposes dummy basic auth is turned on. In Swagger go to "Authorize" and login using arbitrary user name with password "*b*".
+
+Aleternatively you can download pre-built binary archive of your choice from [Releases](../../releases/latest), extract it somewhere on your computer, change into that directory and run in the terminal
+```sh
+./MarBasAPI
+```
+Per default the binary starts production HTTP server (no SSL) on a free port (mostly 5000), i.e. the API endpoints would be reachable via http://localhost:5000/api/marbas. In production mode Swagger is disabled and the only configured user is `reader` with password "*Change_Me*" (can be set in `appsettings.json`). We strongly recommend not using basic authentication with sensitive data, especially when the API is publically accessible - in the future releases we will provide more secure authentication modules.
+
+If you wish that the pre-built executable behaves exactly like the project run by DotNet, set the following environment variables before running `MarBasAPI`
+```sh
+ASPNETCORE_ENVIRONMENT=Development
+ASPNETCORE_URLS=https://localhost:7277
+```
+
+### Example Use
+```sh
+curl -u reader:b "https://localhost:7277/api/marbas/Tree/**"
+curl -u reader:b "https://localhost:7277/api/Role/Current"
+```
 
 ## Using NuGet Packages
 Packages needed to run databroker are published on https://www.nuget.org (keyword: "CraftedSolutions.MarBas"), for utilization example s. https://github.com/Crafted-Solutions/marbas-databroker-pgsql.
--- src/MatBasAPICore/Http/DevelBasicAuthHandler.cs
+++ src/MatBasAPICore/Http/DevelBasicAuthHandler.cs
@@ -1,22 +1,33 @@
 ﻿using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace CraftedSolutions.MarBasAPICore.Http
 {
-    public class DevelBasicAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    public class DevelBasicAuthHandler: AuthenticationHandler<AuthenticationSchemeOptions>
     {
         protected const string ConfigScopeMapRoles = "Auth:MapRoles:";
+        protected const string ConfigScopePrincipals = "Auth:Principals:";
+
+        private static bool _isWarned = false;
 
         protected readonly IConfiguration _configuration;
 
-        public DevelBasicAuthHandler(IConfiguration configuration, IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
+        public DevelBasicAuthHandler(IConfiguration configuration, IHostEnvironment environment, IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder)
+            : base(options, logger, encoder)
         {
             _configuration = configuration;
+            if (!_isWarned && environment.IsProduction() && Logger.IsEnabled(LogLevel.Warning))
+            {
+                Logger.LogWarning("Basic authentication is insecure, consider using different method in production");
+                _isWarned = true;
+            }
         }
 
         protected async override Task<AuthenticateResult> HandleAuthenticateAsync()
@@ -27,7 +38,7 @@ namespace CraftedSolutions.MarBasAPICore.Http
                 var token = authorizationHeader["Basic ".Length..].Trim();
                 var credentialsAsEncodedString = Encoding.UTF8.GetString(Convert.FromBase64String(token));
                 var credentials = credentialsAsEncodedString.Split(':');
-                if (!string.IsNullOrEmpty(credentials[0]) && 2 < credentials[0].Length && "b" == credentials[1])
+                if (!string.IsNullOrEmpty(credentials[0]) && 2 < credentials[0].Length && VerifyCredentials(credentials[0], credentials[1]))
                 {
                     var claims = new[] {
                         new Claim(ClaimTypes.Name, credentials[0]),
@@ -51,6 +62,17 @@ namespace CraftedSolutions.MarBasAPICore.Http
         protected string MapUserRole(string user)
         {
             return _configuration.GetValue($"{ConfigScopeMapRoles}{user}", _configuration.GetValue($"{ConfigScopeMapRoles}*", "Everyone"))!;
+        }
+
+        protected bool VerifyCredentials(string user, string password)
+        {
+            string pwHash = _configuration.GetValue($"{ConfigScopePrincipals}{user}", _configuration.GetValue($"{ConfigScopePrincipals}*", string.Empty));
+            if (!string.IsNullOrEmpty(pwHash))
+            {
+                var msg = Encoding.UTF8.GetBytes(password);
+                return SHA512.HashData(msg).AsSpan().SequenceEqual(Convert.FromHexString(pwHash).AsSpan());
+            }
+            return false;
         }
     }
 }
