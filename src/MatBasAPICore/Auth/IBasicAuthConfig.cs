f2f015fe6a6d371ad3b4f8f16d6337039646f70f
hatelamers <97636078+hatelamers@users.noreply.github.com>
2025-06-07 18:06:10 +0200

    #74 fixed JSON schema for AuthConfig

--- src/MatBasAPICore/Auth/IBasicAuthConfig.cs
+++ src/MatBasAPICore/Auth/IBasicAuthConfig.cs
@@ -1,10 +1,14 @@
-﻿namespace CraftedSolutions.MarBasAPICore.Auth
+﻿using System.Text.Json.Serialization;
+
+namespace CraftedSolutions.MarBasAPICore.Auth
 {
     public interface IBasicAuthConfig: IAuthConfig
     {
     }
 
-    public class BasicAuthConfig: AuthConfig, IBasicAuthConfig
+    public class BasicAuthConfig : AuthConfig, IBasicAuthConfig
     {
+        [JsonIgnore]
+        public override string Schema => "Basic";
     }
 }
